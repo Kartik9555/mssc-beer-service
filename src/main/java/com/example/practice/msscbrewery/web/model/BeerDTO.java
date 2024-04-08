721ca6bd317de12c5bb685550380436a92294448
kartikeshwar.kandpal <kartikeshwar.kandpal@free-now.com>
2024-04-08 09:36:43 +0200

    Added Jackson config for BeerDTO

--- src/main/java/com/example/practice/msscbrewery/web/model/BeerDTO.java
+++ src/main/java/com/example/practice/msscbrewery/web/model/BeerDTO.java
@@ -1,5 +1,6 @@
 package com.example.practice.msscbrewery.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
@@ -24,9 +25,11 @@ public class BeerDTO {
     private Integer version;
 
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
 
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
 
     @NotBlank
@@ -41,7 +44,9 @@ public class BeerDTO {
 
     @NotNull
     @Positive
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private BigDecimal price;
 
+    @Positive
     private Integer quantityOnHand;
 }
